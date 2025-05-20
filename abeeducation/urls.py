e4b7823e39e0b4975666b14842801407ff2e5c9f
aswin <aswink.accolades@gmail.com>
2025-05-20 17:16:20 +0530

    swagger view

--- abeeducation/urls.py
+++ abeeducation/urls.py
@@ -19,7 +19,8 @@ from django.urls import path,include,re_path
 from django.conf.urls.static import static
 from django.conf import settings
 from django.views.static import serve
-from drf_spectacular.views import SpectacularAPIView,SpectacularSwaggerView
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+
 
 admin.site.site_header = "ABE Education Admin"
 admin.site.site_title = "ABE Education Admin"
@@ -33,9 +34,10 @@ urlpatterns = [
     path('api/client/', include('client.urls')),
 
 
-    # swagger
+    ## drf_spectacular
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/schema/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
